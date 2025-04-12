549fb752c0199e52b083f22b7903915630dcee92
HsiaolanKung <hsiaolankung@gmail.com>
2025-04-12 19:21:56 -0400

    needs revision

--- Assets/EnemyPatrol.cs
+++ Assets/EnemyPatrol.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
@@ -10,16 +12,48 @@ public class EnemyPatrol : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Transform currentPoint;
+    public float speed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        currentPoint = GetComponent<Transform>();
+        currentPoint = pointB.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            // flip();
+            currentPoint = pointB.transform;
+        }
     }
+
+    // private void flip()
+    // {
+    //     Vector3 localScale = transform.localScale;
+    //     localScale.x *= -1;
+    //     transform.localScale = localScale;
+    // }
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+    //     Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+    //     Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    // }
 }
